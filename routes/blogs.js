f317b93b2b7d28bbd1cbb86e60f8e9d471626b1a
vishudebaje <debajevishu@gmail.com>
2025-10-04 11:17:22 +0530

    Remove all files from server folder

--- routes/blogs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const { getBlogs, populateBlogs } = require('../controllers/blogsController');
-
-router.get('/blogs', auth, getBlogs);
-router.post('/populate-blogs', populateBlogs);
-
-module.exports = router;
--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const { getHome, populateHome } = require('../controllers/homeController');
-
-router.get('/home', getHome);
-router.post('/populate-home', populateHome);
-
-module.exports = router;
